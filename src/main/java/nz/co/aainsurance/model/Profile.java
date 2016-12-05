9d1eac10fab356863f49f8dc0590713ec0999b79
linlincheng2012 <linlin.cheng2012@gmail.com>
2016-12-06 00:27:15 +1300

    update profile

--- src/main/java/nz/co/aainsurance/model/Profile.java
+++ src/main/java/nz/co/aainsurance/model/Profile.java
@@ -1,40 +1,90 @@
 package nz.co.aainsurance.model;
 
+/**
+ * The Class Profile.
+ */
 public class Profile {
 
+	/** The suburb. */
 	String suburb;
+	
+	/** The street. */
 	String street;
+	
+	/** The datebirth. */
 	String datebirth;
+	
+	/** The sex. */
 	String sex;
 
+	/**
+	 * Gets the suburb.
+	 *
+	 * @return the suburb
+	 */
 	public String getSuburb() {
 		return suburb;
 	}
 
+	/**
+	 * Sets the suburb.
+	 *
+	 * @param suburb the new suburb
+	 */
 	public void setSuburb(String suburb) {
 		this.suburb = suburb;
 	}
 
+	/**
+	 * Gets the street.
+	 *
+	 * @return the street
+	 */
 	public String getStreet() {
 		return street;
 	}
 
+	/**
+	 * Sets the street.
+	 *
+	 * @param street the new street
+	 */
 	public void setStreet(String street) {
 		this.street = street;
 	}
 
+	/**
+	 * Gets the datebirth.
+	 *
+	 * @return the datebirth
+	 */
 	public String getDatebirth() {
 		return datebirth;
 	}
 
+	/**
+	 * Sets the datebirth.
+	 *
+	 * @param datebirth the new datebirth
+	 */
 	public void setDatebirth(String datebirth) {
 		this.datebirth = datebirth;
 	}
 
+	/**
+	 * Gets the sex.
+	 *
+	 * @return the sex
+	 */
 	public String getSex() {
 		return sex;
 	}
 
+	/**
+	 * Sets the sex.
+	 *
+	 * @param sex the new sex
+	 */
 	public void setSex(String sex) {
 		this.sex = sex;
 	}
